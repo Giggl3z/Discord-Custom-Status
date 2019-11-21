4bd3e09bf35da1bb0302ddb9232e8fc44e23575a
Giggl3z <35525972+Giggl3z@users.noreply.github.com>
2019-11-20 21:48:13 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,71 @@
+import random
+import time
+import os
+import requests
+import json
+
+if os.name == "nt":
+    os.system("cls")
+else:
+    os.system("clear")
+
+token = "YOUR TOKEN"
+
+while True:
+  message = "ravioli"
+  message2 = message
+
+  loop = ""
+  for n in message:
+      loop += n
+      status_data = json.dumps(
+          {
+              "custom_status":
+              {
+                  "text": loop
+              }
+          }
+      )
+
+      r = requests.patch("https://discordapp.com/api/v6/users/@me/settings", headers={"Authorization": token, "Content-Type": "application/json"}, data=status_data)
+      print(loop + "\r", end="")
+      time.sleep(0.1)
+
+  message = " " + message + " "
+
+  for n in message:
+      count = 2
+      while count > 0:
+          message += message[0]
+          message = message[1:]
+          count -= 1
+          
+          status_data = json.dumps(
+              {
+                  "custom_status":
+                  {
+                      "text": message
+                  }
+              }
+          )
+
+          r = requests.patch("https://discordapp.com/api/v6/users/@me/settings", headers={"Authorization": token, "Content-Type": "application/json"}, data=status_data)
+          print(message + "\r", end="")
+          time.sleep(0.09)
+
+  message = message2
+
+  for n in range(len(message)):
+      message = message.replace(message[random.randint(0, len(message) - 1)], " ")
+      status_data = json.dumps(
+          {
+              "custom_status":
+              {
+                  "text": message
+              }
+          }
+      )
+
+      r = requests.patch("https://discordapp.com/api/v6/users/@me/settings", headers={"Authorization": token, "Content-Type": "application/json"}, data=status_data)
+      print(message + "\r", end="")
+      time.sleep(0.2)
